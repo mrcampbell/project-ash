daac4436833b2f8560e3d5ec4fadb16bd8de5643
Mike Campbell <mike.campbell@getweave.com>
2020-04-08 10:34:00 -0600

    added actions controller

---
+++ app/controllers/actions_controller.rb
@@ -0,0 +1,6 @@
+class ActionsController < ApplicationController
+  
+  def use_item
+    puts params.inspect
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  # actions
+  post '/actions/items/:item_id', to: 'actions#use_item'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
