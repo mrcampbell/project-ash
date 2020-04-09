74fa79e73ad108f7d8472f9375363c475ae5e93b
Mike Campbell <mike.campbell@getweave.com>
2020-04-09 10:02:11 -0600

    huh.  Got associations working

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,6 +2,8 @@ class TrainersController < ApplicationController
   def index
     puts current_user.inspect
     puts current_trainer.inspect
-    @trainer = Trainer.includes(:pokemons).find(current_trainer.id)
+    @trainer = Trainer.find(current_trainer.id)
+
+    # @trainer = Trainer.includes(:pokemons).includes({:pokemons => :pokemon_moves}).find(current_trainer.id)
   end
 end
