d133fefb1eae8d915ae7e708f6466ec2dceea3c4
Mike Campbell <mike.campbell@getweave.com>
2020-04-11 09:21:56 -0600

    now purchasing products!!!!

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,10 +1,28 @@
+# frozen_string_literal: true
+
 class StoreController < ApplicationController
+
   def index
+    @items_for_sale = helpers.items_for_sale
+    @current_money = current_trainer.money
   end
-  
+
   def purchase
-    puts params[:item_type_id]
-    puts params[:price]
-    redirect_to store_path
+    puts 'PURCHASE'
+    
+    # todo: error check
+    item_order = helpers.items_for_sale[params[:purchase_id].to_sym] 
+
+    puts item_order
+    purchased_item = Item.new(item_type_id: item_order[:type], trainer_id: current_trainer.id)
+    if purchased_item.save 
+      current_money = current_trainer.money
+    end
+    
+    # todo: error check   
+    current_trainer.update(money: current_money - item_order[:buy_price])
+  
+    # todo: this isn't right
+    redirect_to store_path(current_trainer)
   end
 end
--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,2 +1,9 @@
 module StoreHelper
+  def items_for_sale
+    {
+      :purchase_potion => { type: :potion, buy_price: 300, sell_price: 200 },
+      :purchase_antidote => { type: :antidote, buy_price: 250, sell_price: 175 },
+      :purchase_pokeball => { type: :pokeball, buy_price: 100, sell_price: 75 } 
+   }
+  end
 end
